37c0d3b9207962cbcf991260d2af138d80550a94
ahmedsammygeek <ahmedsamigeek@gmail.com>
2023-08-09 20:53:35 +0300

    ...

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -34,7 +34,7 @@
             <div class="row align-items-center">
                 <div class="col-xl-2 col-lg-3 col-md-12">
                     <a href="{{ url('/') }}" class="brand-wrap">
-                        <img class="logo" src="{{ Storage::url('site_assets/images/souq-01.png') }}">
+                        <img style='max-height: 57px; !important' class="logo" src="{{ Storage::url('site_assets/images/souq-01.png') }}">
                     </a> 
                 </div>
                 <div class="col-xl-6 col-lg-5 col-md-6">
