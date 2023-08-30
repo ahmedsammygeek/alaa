481b40e7d182747c52be834d25de9c714245054d
ahmedsammygeek <ahmedsamigeek@gmail.com>
2023-08-30 19:59:04 +0300

    ...

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -79,16 +79,16 @@
                             <div id="carouselExampleIndicators{{ $product->id }}" class="carousel slide"data-ride="carousel">
                                 <ol class="carousel-indicators">
                                     @foreach ($product->images as $product_image)
-                                       <li data-target="#carouselExampleIndicators{{ $product->id }}" data-slide-to="{{ $loop->index }}" class="{{ $loop->index == 0 ? 'active' : '' }}"></li>
+                                    <li data-target="#carouselExampleIndicators{{ $product->id }}" data-slide-to="{{ $loop->index }}" class="{{ $loop->index == 0 ? 'active' : '' }}"></li>
                                     @endforeach
                                 </ol>
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img class="d-block w-100 h-100 " src="{{ Storage::url('products/'.$product->image) }}" alt="First slide">
+                                        <img class="" style='height: 225px !important;' src="{{ Storage::url('products/'.$product->image) }}" alt="First slide">
                                     </div>
                                     @foreach ($product->images as $product_image)
-                                        <div class="carousel-item">
-                                        <img class="d-block w-100 " src="{{ Storage::url('products/'.$product_image->image) }}" alt="Second slide">
+                                    <div class="carousel-item">
+                                        <img class="" style='height: 225px !important;' src="{{ Storage::url('products/'.$product_image->image) }}" alt="Second slide">
                                     </div>
 
                                     @endforeach
@@ -134,22 +134,22 @@
 
             <div class="multiple-items row">
                 @foreach ($best_selling_products as $product)
-                 <div class="col-lg-12">
+                <div class="col-lg-12">
                     <div class="item-box">
                         <div class="item-img">
                             <div id="carouselExampleIndicators{{ $product->id }}" class="carousel slide" data-ride="carousel">
                                 <ol class="carousel-indicators">
                                     @foreach ($product->images as $product_image)
-                                       <li data-target="#carouselExampleIndicators{{ $product->id }}" data-slide-to="{{ $loop->index }}" class="{{ $loop->index == 0 ? 'active' : '' }}"></li>
+                                    <li data-target="#carouselExampleIndicators{{ $product->id }}" data-slide-to="{{ $loop->index }}" class="{{ $loop->index == 0 ? 'active' : '' }}"></li>
                                     @endforeach
                                 </ol>
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img class="d-block w-100 h-100 " src="{{ Storage::url('products/'.$product->image) }}" alt="First slide">
+                                        <img class="d-block w-100 h-100 "  style='height: 225px !important;' src="{{ Storage::url('products/'.$product->image) }}" alt="First slide">
                                     </div>
                                     @foreach ($product->images as $product_image)
-                                        <div class="carousel-item">
-                                        <img class="d-block w-100 " src="{{ Storage::url('products/'.$product_image->image) }}" alt="Second slide">
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100 " style='height: 225px !important;' src="{{ Storage::url('products/'.$product_image->image) }}" alt="Second slide">
                                     </div>
 
                                     @endforeach
@@ -197,22 +197,22 @@
 
             <div class="multiple-items">
                 @foreach ($home_category->products()->latest()->limit(10)->get() as $category_product)
-                 <div class="col-lg-12">
+                <div class="col-lg-12">
                     <div class="item-box">
                         <div class="item-img">
                             <div id="carouselExampleIndicators{{ $category_product->id }}" class="carousel slide" data-ride="carousel">
                                 <ol class="carousel-indicators">
                                     @foreach ($category_product->images as $product_image)
-                                       <li data-target="#carouselExampleIndicators{{ $category_product->id }}" data-slide-to="{{ $loop->index }}" class="{{ $loop->index == 0 ? 'active' : '' }}"></li>
+                                    <li data-target="#carouselExampleIndicators{{ $category_product->id }}" data-slide-to="{{ $loop->index }}" class="{{ $loop->index == 0 ? 'active' : '' }}"></li>
                                     @endforeach
                                 </ol>
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img class="d-block w-100 h-100 " src="{{ Storage::url('products/'.$category_product->image) }}" alt="First slide">
+                                        <img class="d-block w-100 h-100 " style='height: 225px !important;' src="{{ Storage::url('products/'.$category_product->image) }}" alt="First slide">
                                     </div>
                                     @foreach ($category_product->images as $product_image)
-                                        <div class="carousel-item">
-                                        <img class="d-block w-100 " src="{{ Storage::url('products/'.$product_image->image) }}" alt="Second slide">
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100 " style='height: 225px !important;' src="{{ Storage::url('products/'.$product_image->image) }}" alt="Second slide">
                                     </div>
 
                                     @endforeach
@@ -258,45 +258,45 @@
     $(function() {
         $('.multiple-items').slick({
 
-         prevArrow: '<svg width="40" class="prevArrow" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 40C8.9543 40 -2.7141e-06 31.0457 -1.74846e-06 20C-7.8281e-07 8.9543 8.95431 -2.7141e-06 20 -1.74846e-06C31.0457 -7.8281e-07 40 8.9543 40 20C40 31.0457 31.0457 40 20 40ZM16.1206 13.5198C15.7554 13.1055 15.1632 13.1055 14.798 13.5198L9.58704 19.4308C9.22182 19.8451 9.22182 20.5168 9.58704 20.931L14.798 26.8421C15.1632 27.2563 15.7554 27.2563 16.1206 26.8421C16.4858 26.4278 16.4858 25.7561 16.1206 25.3418L12.4912 21.2248L29.6865 21.2248C30.2388 21.2248 30.6865 20.7771 30.6865 20.2248C30.6865 19.6725 30.2388 19.2248 29.6865 19.2248L12.4138 19.2248L16.1206 15.02C16.4858 14.6057 16.4858 13.934 16.1206 13.5198Z" fill="#7C8B9C"/></svg>',
-         nextArrow: '<svg width="40" height="40" class="nextArrow" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 3.49691e-06C31.0457 5.4282e-06 40 8.95431 40 20C40 31.0457 31.0457 40 20 40C8.9543 40 1.56562e-06 31.0457 3.49691e-06 20C5.4282e-06 8.95431 8.95431 1.56562e-06 20 3.49691e-06ZM23.8794 26.4802C24.2446 26.8945 24.8368 26.8945 25.202 26.4802L30.413 20.5692C30.7782 20.1549 30.7782 19.4833 30.413 19.069L25.202 13.1579C24.8368 12.7437 24.2446 12.7437 23.8794 13.1579C23.5142 13.5722 23.5142 14.2439 23.8794 14.6582L27.5088 18.7752L10.3135 18.7752C9.7612 18.7752 9.31348 19.2229 9.31348 19.7752C9.31348 20.3275 9.76119 20.7752 10.3135 20.7752L27.5862 20.7752L23.8794 24.98C23.5142 25.3943 23.5142 26.066 23.8794 26.4802Z" fill="#7C8B9C"/></svg>',
-         dots: false,
-         lazyLoad : 'ondemand' , 
-         accessibility: true , 
-         autoplay: true ,
-         rtl : true , 
-         infinite: true,
-         arrows : true , 
-         speed: 300,
-         slidesToShow: 6,
-         slidesToScroll: 2,
-         responsive: [
-         {
-          breakpoint: 1024,
+           prevArrow: '<svg width="40" class="prevArrow" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 40C8.9543 40 -2.7141e-06 31.0457 -1.74846e-06 20C-7.8281e-07 8.9543 8.95431 -2.7141e-06 20 -1.74846e-06C31.0457 -7.8281e-07 40 8.9543 40 20C40 31.0457 31.0457 40 20 40ZM16.1206 13.5198C15.7554 13.1055 15.1632 13.1055 14.798 13.5198L9.58704 19.4308C9.22182 19.8451 9.22182 20.5168 9.58704 20.931L14.798 26.8421C15.1632 27.2563 15.7554 27.2563 16.1206 26.8421C16.4858 26.4278 16.4858 25.7561 16.1206 25.3418L12.4912 21.2248L29.6865 21.2248C30.2388 21.2248 30.6865 20.7771 30.6865 20.2248C30.6865 19.6725 30.2388 19.2248 29.6865 19.2248L12.4138 19.2248L16.1206 15.02C16.4858 14.6057 16.4858 13.934 16.1206 13.5198Z" fill="#7C8B9C"/></svg>',
+           nextArrow: '<svg width="40" height="40" class="nextArrow" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 3.49691e-06C31.0457 5.4282e-06 40 8.95431 40 20C40 31.0457 31.0457 40 20 40C8.9543 40 1.56562e-06 31.0457 3.49691e-06 20C5.4282e-06 8.95431 8.95431 1.56562e-06 20 3.49691e-06ZM23.8794 26.4802C24.2446 26.8945 24.8368 26.8945 25.202 26.4802L30.413 20.5692C30.7782 20.1549 30.7782 19.4833 30.413 19.069L25.202 13.1579C24.8368 12.7437 24.2446 12.7437 23.8794 13.1579C23.5142 13.5722 23.5142 14.2439 23.8794 14.6582L27.5088 18.7752L10.3135 18.7752C9.7612 18.7752 9.31348 19.2229 9.31348 19.7752C9.31348 20.3275 9.76119 20.7752 10.3135 20.7752L27.5862 20.7752L23.8794 24.98C23.5142 25.3943 23.5142 26.066 23.8794 26.4802Z" fill="#7C8B9C"/></svg>',
+           dots: false,
+           lazyLoad : 'ondemand' , 
+           accessibility: true , 
+           autoplay: true ,
+           rtl : true , 
+           infinite: true,
+           arrows : true , 
+           speed: 300,
+           slidesToShow: 6,
+           slidesToScroll: 2,
+           responsive: [
+           {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+          breakpoint: 600,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
+            slidesToShow: 2,
+            slidesToScroll: 2
         }
     },
     {
-      breakpoint: 600,
+      breakpoint: 480,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 1,
+        slidesToScroll: 1
     }
-},
-{
-  breakpoint: 480,
-  settings: {
-    slidesToShow: 1,
-    slidesToScroll: 1
-}
 }
 
 ]
-     });
+       });
     });
 </script>
 @endsection
