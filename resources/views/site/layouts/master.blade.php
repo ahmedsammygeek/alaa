4509faa5d5912aaf1b2919fc0929c15a53d4c8c8
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-05-01 23:10:26 +0300

    add pixel to head not in body

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -16,6 +16,23 @@ if ($lang == 'ar') {
     <meta http-equiv="cache-control" content="max-age=604800" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title> موقع سوق التجار </title>
+    <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '643601741285719');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=643601741285719&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
     <link href="{{ Storage::url('site_assets/images/favicon.ico') }}" rel="shortcut icon" type="image/x-icon">
     <script src="{{ Storage::url('site_assets/js/jquery-2.0.0.min.js') }}" type="text/javascript"></script>
     <link rel="stylesheet" href="{{ Storage::url('site_assets/css/main.css') }}">
@@ -97,23 +114,7 @@ if ($lang == 'ar') {
    <script src="{{ Storage::url('site_assets/js/all.min.js') }}"></script>
    <script src="{{ Storage::url('site_assets/js/sweetalert2.js') }}"></script>
    <script src="{{ Storage::url('site_assets/js/slick.min.js') }}"></script>
-   <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '643601741285719');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=643601741285719&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
+
    @yield('scripts')
    @stack('scripts')
    <livewire:scripts />
